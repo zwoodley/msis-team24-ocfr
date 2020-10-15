97797e313d478376bdd415dfcd1a6437578cb732
zwoodley <zwoodley@iu.edu>
2020-10-15 14:18:18 -0400

    typo

--- app/public/api/members/index.php
+++ app/public/api/members/index.php
@@ -6,7 +6,7 @@ require 'common.php';
 $db = DbConnection::getConnection();
 
 // Step 2: Create & run the query
-$sql = 'SELECT * FROM person';
+$sql = 'SELECT * FROM ocfr.person';
 $vars = [];
 
 //if (isset($_GET['guid'])) {
