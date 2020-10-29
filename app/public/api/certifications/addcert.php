65815bd473ea7246b1e7a7a124f16cbc5ad97570
Alfred Johnson III <alfjohn@iu.edu>
2020-10-29 02:10:27 -0400

    Removed duplicate php file

--- app/public/api/certifications/addcert.php
+++ app/public/api/certifications/addcert.php
@@ -2,37 +2,6 @@
 
 require 'common.php';
 
-// Step 1: Get a datase connection from our helper class
-$db = DbConnection::getConnection();
-
-// Step 2: Create & run the query
-$sql = 'SELECT m.certificationID as cID, fname, lname,  certName, certAgency, certExp
-        FROM person p join certification c join memberCert m on (p.personID=m.personID) and (c.certificationID=m.certificationID)
-        ORDER BY cID';
-$vars = [];
-
-//if (isset($_GET['guid'])) {
-  // This is an example of a parameterized query
-  // $sql = 'SELECT * FROM Patient WHERE patientGuid = ?';
-  //$vars = [ $_GET['guid'] ];
-//}
-
-$stmt = $db->prepare($sql);
-$stmt->execute($vars);
-
-$patients = $stmt->fetchAll();
-
-// Step 3: Convert to JSON
-$json = json_encode($patients, JSON_PRETTY_PRINT);
-
-// Step 4: Output
-header('Content-Type: application/json');
-echo $json;
-
-<?php
-
-require 'common.php';
-
 // Step 0: Validate the incoming data
 // This code doesn't do that, but should ...
 // For example, if the date is empty or bad, this insert fails.
